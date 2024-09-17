1ffc35ead4a6ff86c20302dbb266fa50d855384a
Priyansh Agarwal <140069604+priyansh201480@users.noreply.github.com>
2024-09-18 00:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
-  // your code here
+  s=s.trim();
+
+	const index = s.indexOf(' ');
+	if(index === -1){
+		return s;
+	}
+	else{
+		return s.substring(0,index);
+	}
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
